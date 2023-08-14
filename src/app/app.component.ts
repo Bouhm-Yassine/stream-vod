1ced50fd6d1f247d21f8cd95c4c2202dfd3042b7
Bouhm-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2023-08-14 15:05:32 +0100

    set up tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,13 @@ import { CommonModule } from '@angular/common';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>  `,
 })
 export class AppComponent {
-  title = 'stream-vod';
 }
