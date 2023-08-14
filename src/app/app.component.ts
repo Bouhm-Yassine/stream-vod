6fe25fb99047ccfae9843d8529eed1bee1fa756b
Bouhm-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2023-08-14 16:36:11 +0100

    add header compo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './core/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   template: `
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>  `,
+  <app-header/>
+
+  `,
 })
 export class AppComponent {
 }
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  <div class="bg-slate-900 h-12 flex items-center p-4">
+    <h1>Github user search AA</h1>
+  </div>
+  `,
+  styles: [
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
